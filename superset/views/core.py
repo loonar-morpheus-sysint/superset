148c48f7256cf5cf3d2356dd2d73294322cf44af
Sandro Cicero <devopsvanilla@outlook.com>
2026-02-03 21:20:40 -0300

    feat: remove access restriction from language pack endpoint

--- superset/views/core.py
+++ superset/views/core.py
@@ -900,7 +900,6 @@ class Superset(BaseSupersetView):
         return json_success(json.dumps(sanitize_datasource_data(datasource.data)))
 
     @event_logger.log_this
-    @has_access
     @expose("/language_pack/<lang>/")
     def language_pack(self, lang: str) -> FlaskResponse:
         # Allow both underscore and dash locale formats
